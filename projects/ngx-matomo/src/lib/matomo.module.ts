a56bfa39a8d84fde82fa4a810ff590adf51f0db8
Arnaud Mouronval <arnaud.mouronval@gmail.com>
2020-05-06 22:45:50 +0200

    feat(injector)!: Inject the Matomo tracker at module import
    The Matomo tracker is now automatically injected in the application when the ngx-Matomo module is being imported. The configuration of the tracker is passed as a JSON object in order to specify the tracker JS script, URL, the different trackers URLs (more than one is now possible), and additional parameters driving the first actions of the tracker(s).
    BREAKING CHANGE: since the injection is now automatic, the old way (0.x) of injecting the tracker manually is no longer compatible with this library.

--- projects/ngx-matomo/src/lib/matomo.module.ts
+++ projects/ngx-matomo/src/lib/matomo.module.ts
@@ -1,11 +1,54 @@
-import { NgModule } from '@angular/core';
+import { NgModule, ModuleWithProviders, Inject, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+
+import {
+  MATOMO_CONFIGURATION,
+  MatomoModuleConfiguration,
+  defaultConfiguration,
+} from './matomo-configuration';
 import { MatomoInjector } from './matomo-injector.service';
 import { MatomoTracker } from './matomo-tracker.service';
 
+/**
+ * Angular module encapsulating Matomo features.
+ */
 @NgModule({
   declarations: [],
   imports: [],
   exports: [],
-  providers: [MatomoInjector, MatomoTracker]
+  providers: [MatomoInjector, MatomoTracker],
 })
-export class MatomoModule {}
+export class MatomoModule {
+  /**
+   * Creates an instance of Matomo module.
+   *
+   * @param platformId Angular platform provided by DI.
+   * @param matomoInjector Instance of MatomoInjector provided by DI.
+   */
+  constructor(@Inject(PLATFORM_ID) private platformId, private matomoInjector: MatomoInjector) {
+    // Warn if module is not being loaded by a browser.
+    if (!isPlatformBrowser(this.platformId)) {
+      console.warn(`ngx-Matomo does not support server platform`);
+    }
+    // Inject the Matomo script and create trackers.
+    this.matomoInjector.init();
+  }
+
+  /**
+   * Use this method in your root module to provide the MatomoTracker service.
+   */
+  static forRoot(configuration?: Partial<MatomoModuleConfiguration>): ModuleWithProviders {
+    return {
+      ngModule: MatomoModule,
+      providers: [
+        {
+          provide: MATOMO_CONFIGURATION,
+          useValue: !!configuration
+            ? { ...defaultConfiguration, ...configuration }
+            : defaultConfiguration,
+        },
+        MatomoTracker,
+      ],
+    };
+  }
+}
--- projects/ngx-matomo/src/public-api.ts
+++ projects/ngx-matomo/src/public-api.ts
@@ -2,6 +2,7 @@
  * Public API Surface of ngx-matomo
  */
 
+export * from './lib/matomo-configuration';
 export * from './lib/matomo-injector.service';
 export * from './lib/matomo-tracker.service';
 export * from './lib/matomo.module';
