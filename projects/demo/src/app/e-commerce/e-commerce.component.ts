b1f4c9ea3f688af6446e887a1241e6b7d8cc4f43
Arnaud Mouronval <arnaud.mouronval@gmail.com>
2022-06-17 16:39:56 +0200

    chore(demo): Switch to Pico.css framework

--- projects/demo/src/app/e-commerce/e-commerce.component.ts
+++ projects/demo/src/app/e-commerce/e-commerce.component.ts
@@ -21,10 +21,7 @@ export class ECommerceComponent implements OnInit {
       id: 201,
       name: 'Nulla',
       price: 207,
-      description:
-        'Culpa sed tenetur incidunt quia veniam sed mollitia exercitationem. ' +
-        'Laboriosam reprehenderit laborum pariatur ea rem qui inventore. In asperiores dignissimos temporibus et. ' +
-        'Beatae consequatur corrupti nam praesentium.',
+      description: 'You really want to know what Nulla is?',
       category: 'Food',
       review: 78,
     },
@@ -32,9 +29,7 @@ export class ECommerceComponent implements OnInit {
       id: 202,
       name: 'Corporis',
       price: 271,
-      description:
-        'Nam incidunt blanditiis odio inventore. Nobis voluptatum quibusdam laboriosam a numquam. ' +
-        'Delectus sequi ipsa possimus ratione repellendus quibusdam. Molestiae fuga laudantium natus dolorem.',
+      description: 'You really want to know what Corporis is?',
       category: 'Clothing',
       review: 67,
     },
@@ -42,17 +37,15 @@ export class ECommerceComponent implements OnInit {
       id: 203,
       name: 'Minus',
       price: 295,
-      description:
-        'Quod reiciendis aspernatur ipsum cum debitis. Quisquam tempore doloremque quo ipsum ipsa tempora. ' +
-        'Dignissimos qui ex ad facilis. Quo sequi recusandae eveniet autem ducimus nam.',
+      description: 'You really want to know what Minus is?',
+      category: 'Food',
       review: 116,
     },
     {
       id: 204,
       name: 'Qui',
       price: 280,
-      description:
-        'Occaecati dolore assumenda facilis error quaerat. Rem harum alias cum eum quam corporis. Esse numquam vero facilis labore.',
+      description: 'You really want to know what Qui is?',
       category: 'Food',
       review: 78,
     },
@@ -60,9 +53,7 @@ export class ECommerceComponent implements OnInit {
       id: 206,
       name: 'Est',
       price: 296,
-      description:
-        'Aut consequatur fugit ut voluptates fugit numquam vero velit. ' +
-        'Distinctio minima quo nesciunt maiores voluptatem dolorum. Doloribus quam nisi molestiae nostrum iure sint debitis.',
+      description: 'You really want to know what Est is?',
       category: 'Clothing',
       review: 107,
     },
@@ -70,10 +61,7 @@ export class ECommerceComponent implements OnInit {
       id: 208,
       name: 'Ratione',
       price: 104,
-      description:
-        'Occaecati nam laudantium est quos. Fuga molestias facere consequatur sapiente cum reprehenderit quibusdam. ' +
-        'Earum omnis ipsum numquam facilis perspiciatis architecto. Iste reiciendis minus distinctio corrupti eos. ' +
-        'Excepturi ut sequi id blanditiis exercitationem.',
+      description: 'You really want to know what Ratione is?',
       category: 'Cosmetic',
       review: 104,
     },
@@ -81,9 +69,7 @@ export class ECommerceComponent implements OnInit {
       id: 209,
       name: 'Similique',
       price: 262,
-      description:
-        'Autem blanditiis similique saepe excepturi at error. Fugit qui accusantium expedita. ' +
-        'Illo similique suscipit sunt magni eos est.',
+      description: 'You really want to know what Similique is?',
       category: 'Clothing',
       review: 44,
     },
@@ -91,9 +77,7 @@ export class ECommerceComponent implements OnInit {
       id: 210,
       name: 'Molestias',
       price: 145,
-      description:
-        'Deserunt ad ducimus recusandae praesentium. Repudiandae officia aliquam quas mollitia. ' +
-        'Voluptatum ipsam iure eos debitis asperiores iusto repudiandae occaecati. Neque itaque sit totam sunt aspernatur at placeat.',
+      description: 'You really want to know what Molestias is?',
       category: 'Food',
       review: 95,
     },
@@ -101,10 +85,7 @@ export class ECommerceComponent implements OnInit {
       id: 211,
       name: 'Modi',
       price: 228,
-      description:
-        'Iure similique perferendis quia optio provident asperiores ad. Perferendis id voluptatibus impedit.' +
-        'Rerum totam quam distinctio eligendi omnis id cumque voluptatem. Et qui odio atque voluptatibus consectetur vel aut. ' +
-        'Qui voluptatem deleniti laboriosam nulla temporibus.',
+      description: 'You really want to know what Modi is?',
       category: 'Cosmetic',
       review: 153,
     },
@@ -112,9 +93,7 @@ export class ECommerceComponent implements OnInit {
       id: 212,
       name: 'Voluptatibus',
       price: 172,
-      description:
-        'Cum aperiam sapiente non magni sequi facere. Et nihil soluta illum ipsum fuga vero. ' +
-        'Magnam nihil quasi illo laudantium pariatur dignissimos. Est officiis quidem fuga dolorem.',
+      description: 'You really want to know what Voluptatibus is?',
       category: 'Food',
       review: 29,
     },
@@ -122,10 +101,7 @@ export class ECommerceComponent implements OnInit {
       id: 213,
       name: 'Sapiente',
       price: 100,
-      description:
-        'Totam repudiandae assumenda facilis quod suscipit repellat delectus eligendi. ' +
-        'Nihil repellendus officiis officia distinctio aperiam dolorem veritatis culpa. ' +
-        'Ab natus doloremque alias dolores deleniti a accusamus.',
+      description: 'You really want to know what Sapiente is?',
       category: 'Cosmetic',
       review: 200,
     },
@@ -133,9 +109,7 @@ export class ECommerceComponent implements OnInit {
       id: 214,
       name: 'Alias',
       price: 152,
-      description:
-        'At non doloribus alias optio delectus sit. Aperiam officiis soluta molestias asperiores similique reiciendis pariatur. ' +
-        'Ab dignissimos iure voluptates error temporibus. Velit ullam quod fugiat molestias nisi explicabo blanditiis.',
+      description: 'You really want to know what Alias is?',
       category: 'Clothing',
       review: 68,
     },
@@ -143,9 +117,7 @@ export class ECommerceComponent implements OnInit {
       id: 215,
       name: 'Non',
       price: 189,
-      description:
-        'Eligendi rem perspiciatis quas accusamus. Consequatur perferendis placeat qui deleniti amet commodi harum reprehenderit. ' +
-        'At quibusdam harum numquam quo.',
+      description: 'You really want to know what Non is?',
       category: 'Food',
       review: 86,
     },
@@ -153,8 +125,7 @@ export class ECommerceComponent implements OnInit {
       id: 216,
       name: 'Quaerat',
       price: 280,
-      description:
-        'Nisi eos aspernatur exercitationem eius architecto dignissimos. Nam recusandae repellat saepe hic.',
+      description: 'You really want to know what Quaerat is?',
       category: 'Food',
       review: 135,
     },
