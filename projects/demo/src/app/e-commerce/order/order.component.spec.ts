575c9a903cbab15a40d10d26a7dd1a2f4053bebf
Arnaud Mouronval <arnaud.mouronval@gmail.com>
2023-06-26 14:16:46 +0200

    chore(demo): Fix OrderComponent testing

--- projects/demo/src/app/e-commerce/order/order.component.spec.ts
+++ projects/demo/src/app/e-commerce/order/order.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { MATOMO_CONFIGURATION, MatomoTracker } from 'ngx-matomo';
 
@@ -28,6 +29,7 @@ describe('OrderComponent', () => {
         MatomoTracker,
       ],
       declarations: [OrderComponent, ArticleComponent],
+      imports: [RouterTestingModule.withRoutes([])],
     });
     fixture = TestBed.createComponent(OrderComponent);
     component = fixture.componentInstance;
