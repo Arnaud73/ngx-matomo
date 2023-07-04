8448dd3f81cdeaa735d03be131fffd96917db624
Arnaud Mouronval <arnaud.mouronval@gmail.com>
2023-07-04 20:18:00 +0200

    fix: Fix incorrect declaration of global window

--- projects/ngx-matomo/src/public-api.ts
+++ projects/ngx-matomo/src/public-api.ts
@@ -1,7 +1,15 @@
+declare global {
+  /**
+   * Extend Window interface in order to introduce the Matomo _paq attribute
+   */
+  interface Window {
+    _paq: any;
+  }
+}
+
 /*
  * Public API Surface of ngx-matomo
  */
-
 export { MatomoConfiguration, MATOMO_CONFIGURATION } from './lib/matomo-configuration';
 export * from './lib/matomo-injector.service';
 export * from './lib/matomo-tracker.service';
